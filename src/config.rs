633251ee148970e6382412c3655b7e17334e1782
Kafva <Martensson.Jonas98@gmail.com>
2022-07-11 10:55:02 +0200

    Global config with one_cell

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,79 @@
-//=== Default values ===//
-pub static DEBUG: bool = false;
+use once_cell::sync::OnceCell;
+use clap::Parser;
+
+// A `static` lifetime infers that a variable will be defined in 
+// the RO section of a binary
+pub const COOKIE_DB_NAMES: &'static [&str] = &[
+    "Cookies",
+    "Safe Browsing Cookies",
+    "cookies.sqlite"
+];
+
+pub const SEARCH_DIRS: &'static [&str] = &[
+    ".mozilla/firefox",
+    ".config/chromium",
+    ".config/BraveSoftware/Brave-Browser",
+
+    "Library/Application Support/Firefox/Profiles",
+    "Library/Application Support/Chromium",
+    "Library/Application Support/BraveSoftware/Brave-Browser",
+
+    "AppData/Roaming/Mozilla/Firefox/Profiles",
+
+    "Library/Application Support/Firefox",
+    "Library/Application Support/Chromium",
+    "Library/Application Support/BraveSoftware/Brave-Browser"
+];
+
+
+//=== Argument parsing ===//
+#[derive(Parser, Debug)]
+#[clap(version = "1.0", author = "Kafva <https://github.com/Kafva>", 
+  about = "Cookie manager")]
+pub struct Args {
+    /// List all cookies across all browsers
+    #[clap(short, long)]
+    list: bool,
+
+    /// Quiet mode, only print the domains for each cookie when using '-l'
+    #[clap(short, long)]
+    quiet: bool,
+
+    /// Debug mode
+    #[clap(short, long)]
+    debug: bool
+}
+
+
+//=== Config object ===//
+#[derive(Debug)]
+pub struct Config {
+    pub err_exit: i32,
+    pub debug: bool,
+    pub quiet: bool
+}
+
+impl Config {
+    /// Used to initialise a new config object
+    pub fn from_args(args: &Args) -> Self {
+        Config { 
+            err_exit: 1,
+            debug: args.debug,
+            quiet: args.quiet
+        }
+    }
+    /// Used to access the global config object in the program
+    pub fn global() -> &'static Self {
+        CONFIG.get().expect("Initialised config object")
+    }
+}
+
+/// Safe one-shot initalisation of a global
+///  https://docs.rs/once_cell/1.4.0/once_cell/
+pub static CONFIG: OnceCell<Config> = OnceCell::new();
+
+
+//=== Types ===//
 
 /// The PartialEq trait allows us to use `matches!` to check
 /// equality between enums
@@ -8,39 +82,6 @@ pub enum DbType {
     Chrome, Firefox, Unknown
 }
 
-// Looking at all files during our search is unnecessarily slow
-pub const COOKIE_DB_NAMES: &'static [&str] = &[
-    "Cookies",
-    "Safe Browsing Cookies",
-    "cookies.sqlite"
-];
-
-// A `static` lifetime infers that a variable will be defined in 
-// the RO section of a binary
-pub const MACOS_SEARCH_PATHS: &'static [&str] = &[
-    "Library/Application Support/Firefox",
-    "Library/Application Support/Chromium",
-    "Library/Application Support/BraveSoftware/Brave-Browser"
-];
-
-pub const GENERIC_SEARCH_PATHS: &'static [&str] = &[
-    ".mozilla/firefox",
-    ".config/chromium"
-];
-
-
-//=== Config object ===//
-pub struct Config {
-    pub err_exit: i32,
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        Config { 
-            err_exit: 1,
-        }
-    }
-}
 
 //=== Macros ===//
 #[macro_export]
@@ -55,11 +96,9 @@ macro_rules! errln {
 macro_rules! debugln {
     // Match one or more expressions to this arm
     ( $($x:expr),* ) => (
-        if config::DEBUG {
+        if CONFIG::global().debug {
             print!("\x1b[34m!>\x1b[0m ");
             println!($($x)*);
         }
     )
 }
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,40 @@
-use std::process;
-use clap::Parser;
+//=== Package imports ===//
 use walkdir::WalkDir;
+use clap::Parser;
 
-//=== Project level imports ===//
+//=== Project imports ===//
 mod config;
 mod funcs;
-use crate::config::{Config,GENERIC_SEARCH_PATHS,MACOS_SEARCH_PATHS,COOKIE_DB_NAMES,DbType,DEBUG};
+use crate::config::{Args,Config,CONFIG,SEARCH_DIRS,COOKIE_DB_NAMES,DbType};
 use crate::funcs::is_cookie_db;
 
-#[derive(Parser, Debug)]
-#[clap(version = "1.0", author = "Kafva <https://github.com/Kafva>", 
-  about = "Cookie manager")]
-struct Args {
-    /// List all cookies across all browsers
-    #[clap(short, long)]
-    list: bool,
-
-    /// Quiet mode, only print the domains for each cookie when using '-l'
-    #[clap(short, long)]
-    quiet: bool
-}
-
 fn main() -> Result<(),()> {
-    let config = Config::default();
+    // Load configuration from argv into a global
     let args: Args = Args::parse();
-    if DEBUG {println!("quiet: {} list: {}\n", args.quiet, args.list);}
-    // https://docs.rs/once_cell/1.4.0/once_cell/
-    // DEBUG = args.quiet;
+    let cfg = Config::from_args(&args);
+    CONFIG.set(cfg).unwrap();
 
-    let home = std::env::var("HOME").unwrap();
-
-    // Go through default paths for each OS and
-    // save a list of all cookie dbs
-    let search_roots:&'static[&'static str] = match std::env::consts::OS {
-        "linux" | "freebsd" => {
-            GENERIC_SEARCH_PATHS
-        },
-        "macos" => {
-            MACOS_SEARCH_PATHS
-        },
-        other => {
-            eprintln!("Unsupported OS: {}", other);
-            process::exit(config.err_exit)
-        }
+    // WSL support
+    let home: String = if std::fs::metadata("/mnt/c/Users").is_ok() { 
+        format!("/mnt/c/Users/{}", std::env::var("USER").unwrap())
+    } else {
+        std::env::var("HOME").unwrap()
     };
 
-    // https://rust-lang-nursery.github.io/rust-cookbook/file/dir.html
-    for search_root in search_roots {
-        if DEBUG {println!("== {search_root} ==");}
-        for entry in WalkDir::new(format!("{home}/{search_root}")).follow_links(false)
+    for search_dir in SEARCH_DIRS {
+        // 'home' needs to be cloned since it is referenced anew in each iteration
+        let search_path: String = home.to_owned() + search_dir;
+
+        // We pass a reference of `search_path` since
+        // we want to retain ownership of the variable for later use
+        for entry in WalkDir::new(&search_path).follow_links(false)
            .into_iter().filter_map(|e| e.ok()) {
             // By filtering on `e.ok()` inaccessible paths are skipped silently
             let f_name = entry.file_name().to_str().unwrap();
 
             if entry.file_type().is_file() && COOKIE_DB_NAMES.contains(&f_name) {
                 let f_path = entry.path().to_str().unwrap();
-                debugln!("Opening: {f_path}");
+                //debugln!(CONFIG.debug, "Opening: {f_path}");
                 let db_type = is_cookie_db(&(entry.path())).unwrap_or_else(|_| {
                     // errln!("Error reading '{f_name}': {err}");
                     return DbType::Unknown;
@@ -65,6 +44,9 @@ fn main() -> Result<(),()> {
                 }
             }
         }
+
+        if Config::global().debug {println!("== {search_path} ==");}
+
     }
 
     return Ok(());
